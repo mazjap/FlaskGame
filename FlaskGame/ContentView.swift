95e33ab79cccc5d8676379b12dd5cfee4a48dec3
Jordan Christensen <jordan.c4922@gmail.com>
2020-12-16 22:27:25 -0700

    Initial Commit

---
+++ FlaskGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlaskGame
+//
+//  Created by Jordan Christensen on 12/16/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlaskGame/FlaskGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlaskGameApp.swift
+//  FlaskGame
+//
+//  Created by Jordan Christensen on 12/16/20.
+//
+
+import SwiftUI
+
+@main
+struct FlaskGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
