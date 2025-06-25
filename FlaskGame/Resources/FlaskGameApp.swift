6daaa9d2f0c6e607b2895cf8109d5eca8a6a8227
Jordan C <jordan.c4922@gmail.com>
2025-06-25 15:07:45 -0600

    Add AdDisclosures so the user knows whats up with their data, improve logging and fix potential error states with ads

--- FlaskGame/Resources/FlaskGameApp.swift
+++ FlaskGame/Resources/FlaskGameApp.swift
@@ -3,22 +3,48 @@ import GoogleMobileAds
 
 // MARK: - Application's entry point
 
+enum TrackingDisclosureStage: String, CaseIterable {
+    case displayingATTInformation
+    case presentingATTAuth
+    case displayingUMPInformation
+    case presentingUMPAuth
+    case complete
+    
+    var isATT: Bool {
+        switch self {
+        case .displayingATTInformation, .presentingATTAuth: true
+        default: false
+        }
+    }
+    
+    var isUMP: Bool {
+        switch self {
+        case .displayingUMPInformation, .presentingUMPAuth: true
+        default: false
+        }
+    }
+}
+
 @main
 struct FlaskGameApp: App {
     @Environment(\.scenePhase) private var scenePhase
     @Namespace private var nspace
     @StateObject private var settings = SettingsController()
     @StateObject private var flasks = FlaskController()
+    @AppStorage(.adDisclosureStage) private var disclosureStage: TrackingDisclosureStage = .displayingATTInformation
     
     init() {
         // Hides home indicator for app store screenshots (in debug only)
         UIViewController.swizzleIndicator()
         
-        MobileAds.shared.requestConfiguration.testDeviceIdentifiers = ["8e22988cfc882ab43b0112b8a514d7d0"]
+        // Configure test devices
+        MobileAds.shared.requestConfiguration.testDeviceIdentifiers = ["06e99044b53a4003ade2a294e299fe0d"]
+        
+        // Start Google Mobile Ads
         MobileAds.shared.start { status in
             status.adapterStatusesByClassName.forEach { (key, value) in
-                guard value.state == .notReady else { return }
                 
+                guard value.state == .notReady else { return }
                 nslog("\(key) is not ready. Latency: \(value.latency)")
             }
         }
@@ -26,12 +52,21 @@ struct FlaskGameApp: App {
     
     var body: some Scene {
         WindowGroup {
-            GameView(flasks: flasks, settings: settings, namespace: nspace)
-                .onChange(of: scenePhase) { _ in
-                    if scenePhase == .background {
-                        flasks.save()
-                    }
+            ZStack {
+                if disclosureStage == .complete {
+                    GameView(flasks: flasks, settings: settings, namespace: nspace)
+                } else {
+                    AdConsentView(disclosureStage: $disclosureStage)
                 }
+            }
+            .onChange(of: scenePhase) { _ in
+                if scenePhase == .background {
+                    flasks.save()
+                }
+            }
+            .onChange(of: disclosureStage) { _ in
+                nslog("Disclosure stage changed to: \(disclosureStage)")
+            }
         }
     }
 }
